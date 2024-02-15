bf2a63dd4de3c6aa885d055a43255c52067b2861
Joel Mora <joeljmorab@gmail.com>
2024-02-15 11:44:53 -0300

    instalando firebase para autorizar desde el mismo

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
-
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 createApp(App).use(router).mount('#app')
