cd2c913ee8b107951c8226e6d0b1ebc311b89094
Joel Mora <joeljmorab@gmail.com>
2024-02-15 12:30:15 -0300

    Login con firebase, creado vista firebase, prueba con exito de logueo con firebase

--- src/main.ts
+++ src/main.ts
@@ -6,4 +6,17 @@ import "bootstrap"
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
+const firebaseConfig = {
+    apiKey: process.env.VUE_APP_FB_API_KEY,
+    authDomain: "auth-vue-16374.firebaseapp.com",
+    projectId: "auth-vue-16374",
+    storageBucket: "auth-vue-16374.appspot.com",
+    messagingSenderId: "347061994620",
+    appId: "1:347061994620:web:21ddae290e243d688ff5f9",
+    measurementId: "G-1QJXZEGM1F"
+  };
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 createApp(App).use(router).mount('#app')
---
+++ src/views/FirebaseView.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <form>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="input" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="email">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" v-model="password">
+            </div>
+    
+            <button type="submit" class="btn btn-primary" @click.prevent="login()">Login</button>
+    
+        </form>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import { ref } from "vue";
+    import {getAuth, signInWithEmailAndPassword} from "firebase/auth"
+
+    let email = ref("")
+    let password = ref("")
+    // let jwt:Ref<string> = ref("")
+        
+    const login = () =>{
+        const auth = getAuth()
+        signInWithEmailAndPassword(auth, email.value, password.value).then(()=>{
+        alert("exito")
+    })
+    .catch((error) => {
+        alert(error)
+    })
+    }
+    
+    
+
+</script>
+
+<style scoped>
+</style>
