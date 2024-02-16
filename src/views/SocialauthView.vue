137ae0f31c2c102a5bef63a8a0f94b5780778c24
Joel Mora <joeljmorab@gmail.com>
2024-02-16 11:49:18 -0300

    ingreso con twitter agregado, ingreso con facebook detenido porque fuck facebook

--- src/views/SocialauthView.vue
+++ src/views/SocialauthView.vue
@@ -3,21 +3,41 @@
         <h1>Social login</h1>
     
         <button class="btn btn-primary" @click="loginGoogle()">Gogle</button>
+        <button class="btn btn-primary" @click="loginTwitter()">X</button>
     
     </div>
 </template>
 
 <script lang="ts" setup>
     import { ref } from "vue";
-    import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+    import { getAuth, signInWithPopup, GoogleAuthProvider, TwitterAuthProvider  } from "firebase/auth";
 
 
-    const provider = new GoogleAuthProvider();
-        
+    
     const loginGoogle = () =>{
+        const provider = new GoogleAuthProvider();
         const auth = getAuth()
-        signInWithPopup(auth, provider).then(()=>{
-        alert("exito")
+        signInWithPopup(auth, provider)
+        .then((result:any)=>{
+            const credentials = GoogleAuthProvider.credentialFromResult(result)
+            console.log(result)
+            const token = credentials
+            alert("exito")
+        })
+        .catch((error) => {
+        console.log(error)
+    })
+    }
+    
+    const loginTwitter = () =>{
+        const provider = new TwitterAuthProvider ();
+        const auth = getAuth()
+        signInWithPopup(auth, provider)
+        .then((result:any)=>{
+            const credentials = TwitterAuthProvider.credentialFromResult(result)
+            console.log(result)
+            const token = credentials
+            alert("exito")
     })
     .catch((error) => {
         console.log(error)
