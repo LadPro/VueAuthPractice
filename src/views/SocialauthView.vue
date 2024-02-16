ead88623a678e018339bff20bce554265d98e67a
Joel Mora <joeljmorab@gmail.com>
2024-02-16 11:58:03 -0300

    agregando login con github

--- src/views/SocialauthView.vue
+++ src/views/SocialauthView.vue
@@ -4,13 +4,14 @@
     
         <button class="btn btn-primary" @click="loginGoogle()">Gogle</button>
         <button class="btn btn-primary" @click="loginTwitter()">X</button>
+        <button class="btn btn-primary" @click="loginGithub()">GitHub</button>
     
     </div>
 </template>
 
 <script lang="ts" setup>
     import { ref } from "vue";
-    import { getAuth, signInWithPopup, GoogleAuthProvider, TwitterAuthProvider  } from "firebase/auth";
+    import { getAuth, signInWithPopup, GoogleAuthProvider, TwitterAuthProvider, GithubAuthProvider  } from "firebase/auth";
 
 
     
@@ -35,6 +36,20 @@
         signInWithPopup(auth, provider)
         .then((result:any)=>{
             const credentials = TwitterAuthProvider.credentialFromResult(result)
+            console.log(result)
+            const token = credentials
+            alert("exito")
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+    }
+    const loginGithub= () =>{
+        const provider = new GithubAuthProvider ();
+        const auth = getAuth()
+        signInWithPopup(auth, provider)
+        .then((result:any)=>{
+            const credentials = GithubAuthProvider.credentialFromResult(result)
             console.log(result)
             const token = credentials
             alert("exito")
