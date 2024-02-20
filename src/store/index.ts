e4468fda05bc0b77baedf63ac31462b6220aa4ee
Joel Mora <joeljmorab@gmail.com>
2024-02-20 08:22:00 -0300

    agregando vuex store para autenticar con azure

---
+++ src/store/index.ts
@@ -0,0 +1,56 @@
+import { createStore } from 'vuex'
+import Emitter from "tiny-emitter"
+
+
+export default createStore({
+  state: {
+    jwt: "",
+    error: "",
+    msalInstance : {},
+    emitter: new Emitter.TinyEmitter()
+  },
+  getters: {
+  
+  },
+  mutations: {
+    setMsalInstance(state, value){
+      state.msalInstance = value
+    }
+  },
+  actions: {
+    async login (state:any, email:string, password:string):Promise<boolean>{
+        try{
+
+            const url = "http://127.0.0.1:5000/auth"
+            const res = await fetch(url, {
+                method: "POST",
+                headers:{
+                    'Accept':'application/json',
+                    'Content-Type':'application/json'
+                },
+                body: JSON.stringify({
+                    email:email,
+                    password: password
+                })
+            })
+            const response = await res.json()
+            
+            if ('errors' in response){
+                state.error = "login failed"
+                return false
+            }
+            state.jwt = response.data.access_token
+            return true
+        }
+        catch(error){
+            console.log(error)
+            state.error = "error en el catch"
+            return false
+        }
+        }
+
+  },
+  modules: {
+
+  }
+})
--- src/views/AzureView.vue
+++ src/views/AzureView.vue
@@ -1,29 +1,97 @@
 <template>
-    <div>
-        <h1>Login con Azure</h1>
-        <button class="btn btn-primary" @click="login()">GitHub</button>
+    <div class="content-container">
+      <div class="section content-title-group">
+        <h2 class="title">Welcome to the Azure AD + Storage app with Vue.js</h2>
+      </div>
+      <div v-if="account">
+        <div class="level">
+          <div class="level-item title">
+            You are logged in as {{ account.name }}
+          </div>
+        </div>
+      </div>
+      <div v-else>You need to authenticate to access your Azure Storage data</div>
     </div>
-</template>
+  </template>
 
-<script lang="ts" setup>
+  
+<script>
+
+import { PublicClientApplication } from '@azure/msal-browser';
+
+export default {
+  name: 'HeaderBar',
+  data() {
+    return {
+      account: undefined,
+      github: 'https://github.com/cmatskas',
+      twitter: 'https://twitter.com/christosmatskas',
+      signin: 'https://microsoft.com',
+    };
+  },
+  async created() {
+    this.$msalInstance = new PublicClientApplication(
+      this.$store.state.msalConfig,
+    );
+  },
+  mounted() {
+    const accounts = this.$msalInstance.getAllAccounts();
+    if (accounts.length == 0) {
+      return;
+    }
+    this.account = accounts[0];
+    this.$emitter.emit('login', this.account);
+  },
+  methods: {
+    async SignIn() {
+      await this.$msalInstance
+        .loginPopup({})
+        .then(() => {
+          const myAccounts = this.$msalInstance.getAllAccounts();
+          this.account = myAccounts[0];
+          this.$emitter.emit('login', this.account);
+        })
+        .catch(error => {
+          console.error(`error during authentication: ${error}`);
+        });
+    },
+    async SignOut() {
+      await this.$msalInstance
+        .logout({})
+        .then(() => {
+          this.$emitter.emit('logout', 'logging out');
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+  },
+};
+</script>
+<!-- <script lang="ts" setup>
+    import { useStore } from 'vuex';
+
     import {PublicClientApplication} from '@azure/msal-browser'
-    import { onMounted } from 'vue';
+    import { onMounted, computed } from 'vue';
     import AzureService from '@/services/AzureService';
 
     let account = ""
-    
+    const store = useStore()
+    const msalInstance = computed(()=> store.state.msalInstance)
+    const emitter = computed(()=> store.state.emitter)
     onMounted(()=>{
         const azureService = new AzureService()
-        $msalInstance = new PublicClientApplication(azureService.getMsalConfig().value)
+        store.commit('setMsalInstance',new PublicClientApplication(azureService.getMsalConfig().value))
+        // $msalInstance = new PublicClientApplication(azureService.getMsalConfig().value)
     })
     
     const login = async () => {
-        await $msalInstance
+        await msalInstance.value
             .loginPopup({})
             .then(()=>{
-                const myAccount = $msalInstance.getAllAccount()
+                const myAccount = msalInstance.value.getAllAccount()
                 account = myAccount[0]
-                $emitter.emit('login', account)
+                emitter.value.emit('login', account)
 
             })
             .cath(error =>{
@@ -32,7 +100,7 @@
     }
 
 
-</script>
+</script> -->
 
 <style scoped>
 </style>
